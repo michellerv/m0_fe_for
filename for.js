9556fb56e3624c8e6b3b9b2d3d225b1927334371
Michelle Villagran <mrvillagran16@gmail.com>
2023-04-19 20:23:04 -0600

    Answer challenge 2

--- for.js
+++ for.js
@@ -11,6 +11,10 @@ for (var i = 26; i < 76; i += 2) {
 // Challenge 2
 // Use a for statement to log to the console a complete sentence stating the weather using the Array below:
 var weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
+for (var i = 0; i < weather_options.length; i++ ){
+  console.log("The weather is " + weather_options[i])
+}
+
 
 
 // Challenge 3
